366d3e334f1af9c2ef2615c58736178c972ac531
Sundaram-rathor <121678006+Sundaram-rathor@users.noreply.github.com>
2025-02-18 20:19:46 +0530

    frontend uri

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import cors from 'cors'
 const app = express();
 const PORT = 3000;
 const corsOptions = {
-    origin:['http://localhost:5173','https://brainly-fe-ten.vercel.app/']
+    origin:['https://brainly-fe-ten.vercel.app']
 }
 
 app.use(express.json());
